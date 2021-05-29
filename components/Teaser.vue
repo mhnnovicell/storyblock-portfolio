b3fa711a2adf26b97582be76a3f1096bd98d8a99
mikkel <mikkelhn1@hotmail.com>
2021-05-29 12:15:13 +0200

    Adding alt attribute and title attribute to img element

--- components/Teaser.vue
+++ components/Teaser.vue
@@ -6,7 +6,7 @@
     <img
      
       v-if="blok.image.filename"
-      :src="blok.image.filename" />
+      :src="blok.image.filename" :alt="blok.image.alt" :title="blok.image.alt" />
 
     <h1 class="text-5xl" v-if="blok.headline">{{ blok.headline }}</h1>
     <p class="mt-3" v-if="blok.description">
