14c2e996af9135efbe5b12b5467f850eb9304ebe
mihngeneraxion <mihn@generaxion.dk>
2021-12-17 12:07:00 +0100

    Update Feature.vue
    ADding some new code

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,35 +1,32 @@
 <template>
-  <div
-    v-editable="blok"
-    data-aos="fade-in">
- <nuxt-img
-    v-if="blok.image.filename"
-            class="img-fluid"
-              provider="cloudinary"
-              :src="blok.image.filename.replace('https://res.cloudinary.com/mikkelraev/image/upload', '')"
-              :alt="blok.image.filename.replace('https://res.cloudinary.com/mikkelraev/image/upload', '')"
-              sizes="xs:100vw sm:100vw md:100vw lg:25vw xl:25vw"
-              :modifiers="{ 
-                    format: 'webp',
-            quality: 80,
-            height: 75}"
-            loading="lazy"
-            />
+  <div v-editable="blok" data-aos="fade-in">
+    <nuxt-img
+      v-if="blok.image.filename"
+      class="img-fluid"
+      provider="cloudinary"
+      :src="this.$nuxt.cloudinary.baseURL + blok.image.filename"
+      :alt="this.$nuxt.cloudinary.baseURL + blok.image.filename"
+      :sizes="this.$nuxt.image.screens"
+      :modifiers="{
+        format: 'webp',
+        quality: 80,
+      }"
+      loading="lazy"
+    />
     <h1 class="text-lg">{{ blok.name }}</h1>
     <p class="text-gray-600">
       {{ blok.description }}
     </p>
-   
   </div>
 </template>
- 
+
 <script>
 export default {
   props: {
     blok: {
       type: Object,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 }
 </script>
