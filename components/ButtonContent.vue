237c7af5387d678ce7865b96ef1e68636fabae57
mninozebra <mni@nozebra.dk>
2022-05-02 12:06:22 +0200

    Adding new code to fix buttons

--- components/ButtonContent.vue
+++ components/ButtonContent.vue
@@ -2,20 +2,24 @@
   <div v-editable="blok" class="grid-row" data-aos="fade-in">
     <div class="row">
       <!-- Using value -->
-      <div class="col-6">
-        <h1
-          v-b-toggle="'collapse-1'"
-          class="m-1 collaspeelementfirst"
-          :class="isActive ? null : 'collapsed'"
-          :aria-expanded="isActive ? 'true' : 'false'"
-          @click="isActive = !isActive"
+      <transition>
+        <div
+          class="col-6"
+          :class="secondElementActive === true ? 'd-none' : 'd-flex'"
         >
-          {{ blok.web_headline }}
-        </h1>
-      </div>
+          <a
+            v-b-toggle="'collapse-1'"
+            class="m-1 collaspeelementfirst h1"
+            :aria-expanded="firstElementIsActive ? 'true' : 'false'"
+            @click="firstElementIsActive = !firstElementIsActive"
+          >
+            {{ blok.web_headline }}
+          </a>
+        </div>
+      </transition>
       <!-- Element to collapse -->
       <b-collapse id="collapse-1">
-        <div class="col-12 collapsible-content">
+        <div class="col-12 collapsible-content mt-3 ml-1">
           <p>{{ blok.web_content }}</p>
           <div
             class="col-3 collapsbile-image-container mt-3"
@@ -50,18 +54,24 @@
         </div>
       </b-collapse>
 
-      <div class="col-6">
-        <h1
-          v-b-toggle="'collapse-2'"
-          class="m-1 collaspeelementsecond"
-          :class="{ active: isActive }"
+      <transition>
+        <div
+          class="col-6"
+          :class="firstElementIsActive === true ? 'd-none' : 'd-flex'"
         >
-          {{ blok.design_headline }}
-        </h1>
-      </div>
+          <a
+            v-b-toggle="'collapse-2'"
+            class="m-1 collaspeelementsecond h1"
+            @click="secondElementActive = !secondElementActive"
+            :aria-expanded="secondElementActive ? 'true' : 'false'"
+          >
+            {{ blok.design_headline }}
+          </a>
+        </div>
+      </transition>
       <!-- Element to collapse -->
       <b-collapse id="collapse-2">
-        <div class="col-12 collapsible-content">
+        <div class="col-12 collapsible-content mt-3 ml-1">
           <p>{{ blok.design_content }}</p>
           <div
             class="col-3 collapsbile-image-container mt-3"
@@ -110,24 +120,57 @@ export default {
 
   data() {
     return {
-      isActive: Boolean,
+      firstElementIsActive: false,
+      secondElementActive: false,
       testid: String,
     }
   },
 
-  mounted() {
-    this.$root.$on('bv::collapse::state', (collapseId, isJustShown) => {
-      console.log('collapseId:', collapseId)
-      console.log('isJustShown:', isJustShown)
-      this.isActive = isJustShown
-      this.testid = collapseId
-    })
-  },
+  // mounted() {
+  //   this.$root.$on('bv::collapse::state', (collapseId, isJustShown) => {
+  //     console.log('collapseId:', collapseId)
+  //     console.log('isJustShown:', isJustShown)
+  //     this.firstElementIsActive = isJustShown
+  //     this.testid = collapseId
+  //     this.firstElementIsActiveElement
+  //   })
+  // },
 
-  computed: {
-    isActiveElement: function () {
-      return this.isActive ? 'd-block' : 'd-block'
-    },
-  },
+  // computed: {
+  //   firstElementIsActiveElement: function () {
+  //     console.log("this.firstElementIsActive", this.firstElementIsActive)
+  //           console.log("this.secondElementActive", this.secondElementActive)
+
+  //     return this.firstElementIsActive ? 'd-block' : 'd-block'
+  //   },
+  // },
 }
 </script>
+
+<style scoped lang="scss">
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.collaspeelementfirst,
+.collaspeelementsecond {
+  box-shadow: 0 8px 32px 0 rgb(31 38 135 / 37%);
+  backdrop-filter: blur(13px);
+  padding: 7px;
+
+  &:hover {
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+
+  &:active {
+  }
+}
+</style>
